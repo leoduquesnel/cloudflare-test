e65133c4df00f8e571b2bcf0b4fac56e9a6aed91
leoduquesnel <leo.duquesnel@gmail.com>
2016-04-19 21:54:31 -0700

    add some tests for survay answer controller

--- test/controllers/survey_answers_controller_test.rb
+++ test/controllers/survey_answers_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class SurveyAnswersControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
+  test "should post create" do
+    post :create, {format: 'js', survey_answer: {email_address: 'leo.duquesnel@gmail.com', selected_option: 'option_1'}}
     assert_response :success
+  end
+
+  test "should create a new answer" do
+    count_before = SurveyAnswer.count
+    post :create, {format: 'js', survey_answer: {email_address: 'leo.duquesnel@gmail.com', selected_option: 'option_1'}}
+    assert count_before + 1 == SurveyAnswer.count
+  end
+
+  test "should not create two answers with the same email address" do
+    post :create, {format: 'js', survey_answer: {email_address: 'leo.duquesnel@gmail.com', selected_option: 'option_1'}}
+    count_before = SurveyAnswer.count
+    post :create, {format: 'js', survey_answer: {email_address: 'leo.duquesnel@gmail.com', selected_option: 'option_1'}}
+    assert count_before == SurveyAnswer.count
   end
 
 end
